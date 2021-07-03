68ef79e25b7d4a8ade1f68338a8fb11596722f25
handfriendship <sonwj77@naver.com>
2021-07-03 14:56:27 +0900

    edited104

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def no_ssl_bypass(_driver) :
 # abcdefghijklmnopqr
 # abcdefghijklmnopqrstuvwxyz
 # abcdefghijklmnopqrstuvwxyz
-# abcdefghijklmn
+# abcdefghijklmno
 
 def login(_driver) :
     return
