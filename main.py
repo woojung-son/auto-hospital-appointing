c1b5603dd17146833d89136f6e893ad8fa6e8cce
handfriendship <sonwj77@naver.com>
2021-08-18 23:09:19 +0900

    edited131

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def no_ssl_bypass(_driver) :
 # abcdefghijklmnopqrstuvwxyz
 # abcdefghijklmnopqrstuvwxyz
 # abcdefghijklmnopqrstuvwxyz
-# abcdefghijklmnopqrstuvwxy
+# abcdefghijklmnopqrstuvwxyz
 
 def login(_driver) :
     return
