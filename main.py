a9a1c5dc39f9c1f781f7e54e8ab42a6a6602d3f2
handfriendship <sonwj77@naver.com>
2021-09-05 01:29:29 +0900

    edited253

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def no_ssl_bypass(_driver) :
         print("Bypassed page loaded")
 # abcdefghijklmnopqrstuvwxyz
 # abcdefghijklmnopqrstuvwxyz
-# abcdefghi
+# abcdefghij
 
 def login(_driver) :
     return
